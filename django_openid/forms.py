38d162c6fb6f87c972b39a755fad871cb2d3d77a
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-11-05 03:41:41 +0300

    Configurable OpenID providers list.

--- django_openid/forms.py
+++ django_openid/forms.py
@@ -1,13 +1,20 @@
+import re
+
 from django import forms
 import models
 from django.conf import settings
+from django_openid.settings import DEFAULT_SERVICES
+
+_services = getattr(settings, 'OPENID_SERVICES', DEFAULT_SERVICES)
+
+_service_choices = sorted((service, name) for service, (name, _, _) in _services.iteritems())
+_service_urls = dict((service, url) for service, (_, url, _) in _services.iteritems())
+_service_patterns = list((service, re.compile(pattern or (url % '(?P<user>.*)')).search) for service, (_, url, pattern) in _services.iteritems())
+
 
 class OpenIDInput(forms.MultiWidget):
     def __init__(self, attrs=None):
-        widgets = (forms.Select(choices=(
-                        (u'yandex', u'Yandex'),
-                        (u'livejournal', u'LiveJournal'),
-                        (u'openid', u'OpenID'))),
+        widgets = (forms.Select(choices=_service_choices),
                    forms.TextInput)
         super(OpenIDInput, self).__init__(widgets, attrs)
 
@@ -15,17 +22,14 @@ class OpenIDInput(forms.MultiWidget):
         value = super(OpenIDInput, self).value_from_datadict(data, files, name)
         try:
             service, user = value
-            return {u'yandex': u'http://openid.yandex.ru/%s/',
-                    u'livejournal': u'http://%s.livejournal.com',
-                    }.get(service, u'%s') % user
+            print  _service_urls.get(service, u'%s') % user
+            return _service_urls.get(service, u'%s') % user
         except ValueError:
             return None
 
     def decompress(self, value):
         if value:
-            for service, search in (
-                (u'yandex', re.compile(r'openid.yandex.ru/(?P[a-zA-Z0-9-]+)').search),
-                (u'livejournal', re.compile(r'(?P[a-zA-Z0-9-]+).livejournal.com').search)):
+            for service, search in _service_patterns:
                 result = search(value)
                 if result:
                     return [service, result.group('user')]
---
+++ django_openid/settings.py
@@ -0,0 +1,16 @@
+DEFAULT_SERVICES = {
+    u'livejournal': (u'LiveJournal', u'http://%s.livejournal.com', r'(?P<user>[a-zA-Z0-9-]+).livejournal.com'),
+    u'openid': (u'OpenID', u'%s', r'(?P<user>.*)'),
+    u'myopenid': (u'myOpenID', u'http://%s.myopenid.com/', r''),
+    u'flickr': (u'Flickr', u'http://www.flickr.com/photos/%s', r''),
+    u'yahoo': (u'Yahoo', u'http://me.yahoo.com/%s', r''),
+    u'aol': (u'AOL', u'http://openid.aol.com/%s', r''),
+    u'blogger': (u'Blogger', u'http://%s.blogspot.com/', r''),
+    u'verisign': (u'Verisign', u'http://%s.pip.verisignlabs.com/', r''),
+    u'vidoop': (u'Vidoop', u'http://%s.myvidoop.com/', r''),
+    u'claimid': (u'claimID', u'http://claimid.com/%s', r''),
+    u'technorati': (u'Technorati', u'http://technorati.com/people/technorati/%s', r''),
+    u'vox': (u'Vox', u'http://%s.vox.com/', r''),
+    u'wordpresscom': (u'Wordpress.com', u'http://%s.wordpress.com/', r''),
+}
+
