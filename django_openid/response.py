c0635ff29c47ca8716163aeefe66598adf6dcfd0
Alexander Artemenko <svetlyak.40wt@gmail.com>
2009-01-31 02:26:09 +0300

    Code cleanup

--- django_openid/response.py
+++ django_openid/response.py
@@ -1,39 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader, Context, RequestContext
-from pdb import set_trace
 
 class TemplateResponse(HttpResponse):
-
-    def set_template(self, value):
-#        if not value:
-#            set_trace()
-#        assert(value and 'template argument must not be None')
-        print 'New template value for %s is: %s' % (repr(self), value)
-        #set_trace()
-        self._real_template = value
-    def get_template(self):
-        return self._real_template
-
-    _template = property(get_template, set_template)
-
-#    def _get_context(self):
-#        return self._context
-#    def _set_context(self, value):
-#        print 'NEW CONTEXT: ', value
-#        self._context = value
-#        if value is None:
-#            set_trace()
-    #context = property(_get_context, _set_context)
-
     def __init__(self, template, context, *args, **kwargs):
-        assert(template and 'template argument must not be None')
-        assert(context.get('base_template', None) and 'base_template not given in the context')
         self._template = template
         self._context = context
         self.rendered = False
-        print 'INIT ', self._context
         super(TemplateResponse, self).__init__(*args, **kwargs)
-        assert(self._context.get('base_template', None) and 'base_template not given in the context')
     
     def resolve_template(self, template):
         # Template can be a template object, path-to-template or list of paths
@@ -52,19 +25,9 @@ class TemplateResponse(HttpResponse):
             return Context(context)
     
     def render(self):
-        if not self._template:
-            set_trace()
-        assert(self._template and 'please, setup template to render')
-
         template = self.resolve_template(self._template)
-        assert(template and 'template not found')
-
         context = self.resolve_context(self._context)
-        try:
-            return template.render(context)
-        except Exception, e:
-            raise
-            #set_trace()
+        return template.render(context)
     
     def bake(self):
         """
@@ -74,7 +37,6 @@ class TemplateResponse(HttpResponse):
         able to over-write the content of a response.
         """
         if not self.rendered:
-            print 'BAKE ', self._context
             self._set_content(self.render())
             self.rendered = True
     
@@ -95,10 +57,6 @@ class RequestTemplateResponse(TemplateResponse):
     
     def __init__(self, request, template, context, *args, **kwargs):
         self.request = request
-        assert(template)
-        base_template = context.get('base_template')
-        print base_template
-        set_trace()
         super(RequestTemplateResponse, self).__init__(
             template, context, *args, **kwargs
         )
